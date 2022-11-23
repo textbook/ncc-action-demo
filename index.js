7469061c89737fcd7dfcd22aa3d2caa364e1580c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-11-23 21:18:22 +0000

    Don't destructure the response

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const http = require("@actions/http-client");
 const client = new http.HttpClient();
 
 (async () => {
-  const { message, readBody } = await client.get("https://checkip.amazonaws.com");
-  if (message.statusCode !== http.HttpCodes.OK) {
-    throw new Error(message.statusMessage);
+  const res = await client.get("https://checkip.amazonaws.com");
+  if (res.message.statusCode !== http.HttpCodes.OK) {
+    throw new Error(res.message.statusMessage);
   }
-  const ip = await readBody();
+  const ip = await res.readBody();
   core.setOutput("public-ip", ip);
 })();
